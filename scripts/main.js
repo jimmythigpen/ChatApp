2f3b5df7edaa454c4d661a8b6cd1eaf245eaf06d
Jimmy Thigpen <jmthigpen@me.com>
2015-01-29 16:10:15 -0500

    4PM update

--- scripts/main.js
+++ scripts/main.js
@@ -3,39 +3,57 @@
 
   $(document).ready(function() {
 
-    var $url = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
-    var $user;
-    var $message;
+    var serverURL = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
 
-    $user = "codebabe";
-    $message = "Looking forward to the weekend";
+
+    //
+    // Get Chat Message from input field
+    //
+    $(".name-button").on("click", storeName);
+    $(".message-button").on("click", sendMessage);
+
+    //
+    // Store Username
+    //
+    function storeName() {
+      // event.preventDefault();
+      var userName = $(".user-name-field").val();
+      console.log(userName);
+    }
+
+
+    //
+    // Post message to Chat
+    //
+    function sendMessage() {
+      var newMessage = $(".input-field").val();
+      $.ajax({
+        url: "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats",
+        type: "POST",
+        data: {
+          username: userName,
+          message: newMessage,
+        }
+      }).done(function(data) {
+        console.log(data);
+      });
+    }
+
 
     //
     // Get All Chat Messages From Server
     //
-    $.ajax({
-      url: $url,
-      type: "GET"
-    }).done(function(data) {
-      // console.log(data);
-
-    });
+    // $.ajax({
+    //   url: $url,
+    //   type: "GET"
+    // }).done(function(data) {
+    //   console.log(data);
+    // });
 
     //
     // Make Post On Server
     //
-    $.ajax({
-      url: "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats",
-      type: "POST",
-      data: {
-        message: "Hi!",
-        username: "BillyBob",
-        createdAt: Date.now()
-      }
-    }).done(function(data) {
-      console.log(data);
 
-    });
 
   });
 })();
