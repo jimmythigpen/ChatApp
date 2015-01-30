fd01220ee9abb18b33597da4054b5cd819aee591
Jimmy Thigpen <jmthigpen@me.com>
2015-01-30 07:52:49 -0500

    8AM

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,14 @@
   $(document).ready(function() {
 
     var serverURL = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
-    var userName = '';
+
+    //
+    // Find Username
+    //
+    var userName = prompt("Enter a username");
+    if (userName == null) {
+      userName = "Anonymous";
+    }
 
     var renderMessageTemplate = _.template($('.message-data').text());
     var messageTemplate = $('.messages-list');
@@ -12,8 +19,14 @@
     //
     // Get Chat Message from input field
     //
-    $(".name-button").on("click", storeName);
+    // $(".name-button").on("click", storeName);
     $(".message-button").on("click", sendMessage);
+
+    // $(".name-button").click(function() {
+    //   ($(".message-container").scrollTop(400) + " px");
+    // });
+
+
 
     //
     // Store Username
@@ -27,7 +40,7 @@
     // Post message to Server
     //
     function sendMessage() {
-      var newMessage = $(".input-field").val();
+      var newMessage = $(".text-field").val();
       $.ajax({
         url: "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats",
         type: "POST",
@@ -40,19 +53,56 @@
       });
     }
 
-
     //
-    // Get ALl Messages from server
+    // Get All Messages from server
     //
 
+    // function getMessages() {
+    messageTemplate.empty();
     $.ajax({
       url: serverURL,
       type: "GET"
     }).done(function(messages) {
+      messages = messages.reverse();
       _.each(messages, function(message) {
+
+        if (message.createdAt == null) {
+          message.createdAt = "unknown ago";
+        } else {
+          message.createdAt = moment(message.createdAt).fromNow();
+        }
+
+        if (message.message == null) {
+          message.message = "";
+        }
+
         messageTemplate.append(renderMessageTemplate(message));
+
       });
     });
+    // }
+    // setInterval(getMessages, 5000);
+
+    // $(".messages-container").animate({
+    //   scrollTop: $('.messages-container')[0].scrollHeight
+    // }, 1000);
+    // var interval = setInterval(function(){
+    //   console.log(Date.now());
+    // }, 1000);
+    //
+    // clearInterval(interval);
+
+    // setInterval(function() {
+    //
+    //   $.ajax({
+    //     url: serverURL,
+    //     type: "GET"
+    //   }).done(function(messages) {
+    //     _.each(messages, function(message) {
+    //       messageTemplate.append(renderMessageTemplate(message));
+    //     });
+    //   });
+    // }, 3000);
 
 
   });
